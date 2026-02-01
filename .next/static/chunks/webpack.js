178595bc7869f2a07590752ce32929a872759b30
ianjohndal <ianjohndal5@gmail.com>
2026-02-01 14:31:19 +0800

    Update .gitignore to exclude .next directory; modify layout.tsx to clarify dynamic rendering restrictions for Client Components.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cbaa785b98f7bc01"; }
+/******/ 		__webpack_require__.h = function() { return "6273cd8680a0b84c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
